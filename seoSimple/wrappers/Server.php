57c787e65db4353bf9d528f3492e143bac30a330
Will Smelser <willsmelser@gmail.com>
2013-07-02 18:45:21 -0400

    Added some more wrappers

---
+++ seoSimple/wrappers/Server.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../class/ServerInfo.php';
+
+class Server extends ServerInfo {
+	public function ServerInfo($url){
+		parent::__construct($url);
+	}
+}
+?>
---
+++ seoSimple/wrappers/Social.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once '..//src/seostats.php';
+
+class Social{
+	private $url;
+	private $stats;
+	private $social;
+	privaet $fb;
+	
+	public function Social($url){
+		if(empty($url)) return;
+	
+		$this->stats = new SEOstats($url);
+		$this->social = $this->stats->Social();
+		$this->url = $url;
+	}
+	
+	public function googlePlus(){
+		return $social->getGoogleShares();
+	}
+	
+	public function getFbLikeCount(){
+		return $this->getFb()['like_count'];
+	}
+	
+	public function getFbShareCount(){
+		return $this->getFb()['share_count'];
+	}
+	
+	public function getFbCommentCount(){
+		return $this->getFb()['comment_count'];
+	}
+	
+	public function getTwitterShares(){
+		return $this->social->getTwitterShares();
+	}
+	
+	public function getDeliciousShares(){
+		return $this->social->getDeliciousShares();
+	}
+	
+	public function getDiggShares(){
+		return $this->social->getDiggShares();
+	}
+	
+	public function getLinkedInShares(){
+		return $this->social->getLinkedInShares();
+	}
+	
+	public function getPinterestShares(){
+		return $this->social->getPinterestShares();
+	}
+	
+	public function getStumbleUponShares(){
+		return $this->social->getStumbleUponShares();
+	}
+	
+	public function getVKontakteShares(){
+		return $this->social->getVKontakteShares();
+	}
+	
+	private function getFb(){
+		if(empty($this->fb))
+			$this->fb = $this->social->getFacebookShares();
+			
+		return $this->fb;
+	}
+}
