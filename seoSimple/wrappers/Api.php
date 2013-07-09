f3a96390cf2d167dbeb86e07c0d0740fd153c91e
Will Smelser <willsmelser@gmail.com>
2013-07-08 22:36:42 -0400

    Documentation is done now.

---
+++ seoSimple/wrappers/Api.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This class is a placeholder to define the API.
+ * Make requests as:
+ *  /api/body/<method>?request=<url encoded link>
+ *  method = <class method> [|<class method>] | all
+ *  class method = A method that exists in the parent class
+ * @uses HtmlBodyWrap 
+ * @author Will
+ *
+ */
+class Body extends HtmlBodyWrap{
+	
+}
+
+/**
+ * This class is a placeholder to define the API.
+ * Make requests as:{@example
+ *  /api/head/<method>?request=<url encoded link>
+ *  method = <class method> [|<class method>] | all
+ *  class method = A method that exists in the parent class
+ * }
+ * @uses HtmlHeadWrap
+ * @author Will
+ *
+ */
+class Head extends HtmlHeadWrap{
+
+}
+
+/**
+ * This class is a placeholder to define the API.
+ * Make requests as:{@example
+ *  /api/server/<method>?request=<url encoded link>
+ *  method = <class method> [|<class method>] | all
+ *  class method = A method that exists in the parent class
+ * }
+ * @uses ServerWrap
+ * @author Will
+ *
+ */
+class Server extends ServerWrap{
+
+}
+
+/**
+ * This class is a placeholder to define the API.
+ * Make requests as:{@example
+ *  /api/social/<method>?request=<url encoded link>
+ *  method = <class method> [|<class method>] | all
+ *  class method = A method that exists in the parent class
+ * }
+ * @uses SocialWrap
+ * @author Will
+ *
+ */
+class Social extends SocialWrap{
+
+}
+?>
---
+++ seoSimple/wrappers/README.txt
@@ -0,0 +1,2 @@
+Run this command from the wrapper directory to create documentation:
+$>phpdoc.bat -d . -f ../class/ServerInfo.php,../api/header.php,../config.php,../class/Node.php -t docs
