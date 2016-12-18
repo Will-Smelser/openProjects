55a8edb2526381b1533d49733c36a06eb930e989
Will-Smelser <willsmelser@gmail.com>
2016-12-18 12:28:09 -0500

    Just added some documentation.

--- WorkManager/src/main/java/com/mediocredeveloper/cloud/message/CloudResp.java
+++ WorkManager/src/main/java/com/mediocredeveloper/cloud/message/CloudResp.java
@@ -3,7 +3,9 @@ package com.mediocredeveloper.cloud.message;
 import java.io.Serializable;
 
 /**
- * Created by Will2 on 12/3/2016.
+ * Just used for genernal messaging response.  But a little silly, since if a node does respond saying, then it
+ * most likely would always respond with {@link #YES}.  The only time a {@link #NO} would happen is if the node
+ * cannot be contacted.
  */
 public enum CloudResp implements Serializable {
     YES, NO
